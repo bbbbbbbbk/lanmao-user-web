4ff3aafe09df416d2349d3e59e4a5f889dc5cb5f
moshenglei <moshenglei@icloud.com>
2019-10-16 23:47:40 +0800

    tijiao

--- src/router.js
+++ src/router.js
@@ -3,11 +3,30 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-const routers = [
+const routes = [
     {
-
+        name: 'main',
+        component: () => import('./view/main'),
+        meta: {
+            title: '首页'
+        }
     }
-]
+];
+
+// add route path
+routes.forEach(route => {
+    route.path = route.path || '/' + (route.name || '');
+});
+
+const router = new Router({ routes });
+
+router.beforeEach((to, from, next) => {
+    const title = to.meta && to.meta.title;
+    if (title) {
+        document.title = title;
+    }
+    next();
+});
 
 export {
     router
---
+++ src/view/main/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>Hello World!</div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
